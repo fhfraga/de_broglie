14cae0b494de6d87dbb93cba8fd71d45dec4e8ac
Fernando Henrique Fraga <f.henrique.fraga@gmail.com>
2019-12-18 16:09:48 -0300

    documentação da função de_broglie_lambda

--- core/de_broglie.py
+++ core/de_broglie.py
@@ -1,6 +1,22 @@
 from scipy import constants
 
 
-def de_broglie_lambda (massa, velocidade, cte_planck = constants.h ):
+def de_broglie_lambda(massa, velocidade, cte_planck=constants.h):
+    """Calcula comprimento de onda usando equação de De Broglie
+    
+    Parameters
+    ----------
+    massa : float
+        massa da partícula desejada
+    velocidade : float
+        velocidade da partícula desejada
+    cte_planck : float, opcional
+        Constante de Planck, por padrão constants.h do pacote scipy, cuja unidade é joule por segundo
+    
+    Returns
+    -------
+    float
+        comprimento de onda associado
+    """    
     comp_onda = cte_planck / (massa * velocidade)
     return comp_onda
