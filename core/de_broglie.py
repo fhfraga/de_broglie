a30a272c521e5aed1cf0cb25fe55d0fc76790f01
Fernando Henrique Fraga <f.henrique.fraga@gmail.com>
2019-12-18 22:47:07 -0300

    Funções para achar energia, lambda e espectro eletromagnético de um fóton

--- core/de_broglie.py
+++ core/de_broglie.py
@@ -24,15 +24,52 @@ def de_broglie_lambda(massa, velocidade, cte_planck=constants.h):
 
 
 def energia_de_foton(comp_onda, cte_planck=constants.h, cte_luz=constants.c):
+    """Calcula a energia de um foton usando a equação de Planck-Einstein
+    
+    Parameters
+    ----------
+    comp_onda : float
+        lambda da partícula desejada
+    cte_planck : float, opcional
+        Constante de Planck, por padrão constants.h do pacote scipy, cuja unidade é joule por segundo
+    cte_luz : [type], optional
+        Constante da velocidade da luz no vácua, por padrão constants.c do pacote scipy, cija unidade é metros por segundo--
+    
+    Returns
+    -------
+    float
+        Energia de um fóton
+    """    
     energia = (cte_planck * cte_luz) / comp_onda
     return energia
 
 
 def energia_de_foton_mol (energia_de_foton, avogadro=constants.Avogadro):
+    """Calcula a energia de um foton usando a equação de Planck-Einstein, modificando para um mol com a constante de Avogadro
+    
+    Parameters
+    ----------
+    energia_de_foton : float
+        Energia de um única fóton
+    avogadro : [float], opcional
+        Número de atomos em um mol de substância,por padrão constants.Avogadro do pacote scipy.
+    
+    Returns
+    -------
+    [float]
+        Energia de fótons em um mol de uma determinada substância
+    """    
     energia = energia_de_foton * avogadro
     return energia
 
 def espectro_eletromagnetico(comp_onda):
+    """Espectro eletromagnética associado aos comprimentos de onda encontradas na primeira função
+    
+    Parameters
+    ----------
+    comp_onda : float
+        comprimento de onda encontrado na primera função, dado em metros
+    """    
     if 7* 10**-7 < comp_onda < 10 ** -4:
         print("Infravermelho")
     elif 4*10**-7 <comp_onda < 7 * 10 ** -7:
