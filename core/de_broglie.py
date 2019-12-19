c56e9d18e5f24f5ede1ef7ca2e7249b9abb381e5
Fernando Henrique Fraga <f.henrique.fraga@gmail.com>
2019-12-18 22:07:13 -0300

    Nova função que acha valor de energia para um mol

--- core/de_broglie.py
+++ core/de_broglie.py
@@ -3,7 +3,7 @@ from scipy import constants
 
 def de_broglie_lambda(massa, velocidade, cte_planck=constants.h):
     """Calcula comprimento de onda usando equação de De Broglie
-    
+
     Parameters
     ----------
     massa : float
@@ -12,11 +12,45 @@ def de_broglie_lambda(massa, velocidade, cte_planck=constants.h):
         velocidade da partícula desejada
     cte_planck : float, opcional
         Constante de Planck, por padrão constants.h do pacote scipy, cuja unidade é joule por segundo
-    
+
     Returns
     -------
     float
         comprimento de onda associado
-    """    
+    """
     comp_onda = cte_planck / (massa * velocidade)
     return comp_onda
+
+
+
+def energia_de_foton(comp_onda, cte_planck=constants.h, cte_luz=constants.c):
+    energia = (cte_planck * cte_luz) / comp_onda
+    return energia
+
+
+def energia_de_foton_mol (energia_de_foton, avogadro=constants.Avogadro):
+    energia = energia_de_foton * avogadro
+    return energia
+
+def espectro_eletromagnetico(comp_onda):
+    if 7* 10**-7 < comp_onda < 10 ** -4:
+        print("Infravermelho")
+    elif 4*10**-7 <comp_onda < 7 * 10 ** -7:
+        print("Vísivel")
+    elif 10**-8 < comp_onda < 10 ** -7:
+        print ("Ultravioleta")
+    elif 10**-12< comp_onda < 10 ** -8:
+        print("Raios-X")
+    else:
+        print("Raios Gama")
+
+massa = eval(input("Massa da partícula: "))
+velocidade = eval(input("Velocidade da partícula: "))
+
+x = de_broglie_lambda(massa, velocidade)
+y = energia_de_foton (x)
+
+print(x)
+print(energia_de_foton(x))
+print(energia_de_foton_mol(y))
+print(espectro_eletromagnetico (x))
--- tests/test_de_broglie.py
+++ tests/test_de_broglie.py
@@ -5,5 +5,13 @@ from core import de_broglie
 massa_e = constants.m_e
 
 class TestDeBroglie(unittest.TestCase):
-    def test_e_v4e6 (self):
+    def test_e_v4e6(self):
         self.assertAlmostEqual(de_broglie.de_broglie_lambda(massa_e, 4e6), 1.8184737738719174e-10)
+
+class TestDeEnergia(unittest.TestCase):
+    def test_comp_onda_e(self):
+        self.assertAlmostEqual(de_broglie.energia_de_foton(1.8184737738719174e-10), 1.092369795326876e-15)
+
+class TestDeEnergiaMol(unittest.TestCase):
+    def test_energia_foton(self):
+        self.assertAlmostEqual(de_broglie.energia_de_foton_mol(1.092369795326876e-15), 657840477.5390708)
